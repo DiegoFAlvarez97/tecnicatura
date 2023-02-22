334a89dc77509379886c7550e59fd07324a8f836
= <=>
2023-02-22 23:25:08 +0100

    Commiteamos Clase13

--- Leccion9/test_figura_geometrica.py
+++ Leccion9/test_figura_geometrica.py
@@ -1,17 +1,25 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 
+print('Creacion de objeto clase Cuadrado'.center(50, '_'))
 cuadrado1 = Cuadrado(5, 'Azul')
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
+#cuadrado1.alto = -10
+#cuadrado1.ancho = 7
+#print(cuadrado1.ancho)
+#print(cuadrado1.alto)
 print(f'Calculo del area del cuadrado: {cuadrado1.calcular_area()}')
 
 #MRO = Method Resolution Order
-print(Cuadrado.mro())
+#print(Cuadrado.mro())
 
 print(cuadrado1)
 
 
+print('Creacion de objeto clase Rectangulo'.center(50, '_'))
 rectangulo1 = Rectangulo(3, 6, 'Verde')
+rectangulo1.ancho = 15
 print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
+
+# figura1 = FiguraGeometrica() No se puede instanciar, es abstracta
+print(Cuadrado.mro())
