01e6724b8cf1aa6cfdc4693a2f4ab8fc015c9fbe
= <=>
2023-03-04 13:13:55 +0100

    Comiteamos la Clase 5

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -14,4 +14,17 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        //return resultado;
+        return this.a + this.b;
+    }
+    
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; //El argumento a se le asigna al atributo this.a
+        this.b = b;
+        //return a + b;
+        return sumarConRetorno();
+    }
 }
